deef037e666de7f4676292a7c99049b0c51851b0
shooga0007 <businessexclusiveez@gmail.com>
2023-02-21 06:37:26 -0800

    102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints sum of even valued fibonacci numbers under 4,000,000
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i = 0;
+	long int a = 0, b = 1, next;
+
+	while (i < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
